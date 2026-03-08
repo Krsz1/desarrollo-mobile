0aa1968ac3d41e765093930e72ef54df38cf53f6
Krsz <krsna.gutierrez@uao.edu.co>
2026-03-07 23:31:13 -0500

    help

--- challenges/challenge-03/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.taskmanager',
-  appName: 'Task Manager',
-  webDir: 'dist'
-};
-
-export default config;
