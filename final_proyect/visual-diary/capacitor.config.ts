c902fe7fb7c3980ee58e1051e7f1ab835d092a76
Krss <krsna.gutierrez@uao.edu.co>
2026-05-28 23:40:25 -0500

    androiStudio

--- final_proyect/visual-diary/capacitor.config.ts
+++ final_proyect/visual-diary/capacitor.config.ts
@@ -1,9 +1,12 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'visual-diary',
-  webDir: 'dist'
+  appId: 'com.visualdiary.app',
+  appName: 'VisualDiary',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
